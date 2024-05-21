b84d16b602d1e1b3b16d0432acb835f113ad7594
Varsha Shetty <varshashetty551@gmail.com>
2024-05-21 21:47:30 +0530

    code is working

---
+++ game.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    int Number,guess,nGuesses=1;
+    srand(time(0));
+    Number = rand()%100+1;
+
+     do{
+        
+        printf("Guess the number between 1 and 100:\n");
+        scanf("%d",&guess);
+   
+        if(guess>Number)
+        {
+            printf("Lower number please.....\n");
+        }
+         else if(guess<Number)
+        {
+            printf("Higher number please.....\n");
+        }
+        else
+        {
+            printf("Yayy!!!!....You Got it.\n You took %d attempts",nGuesses);
+        }
+        nGuesses++;
+        
+    }while(guess!=Number);
+
+
+return 0;
+}
